c6ca164024a2d2d944d0cc8e6479ca635fbda732
Dylan DesRosier <dylan@pooltogether.com>
2022-12-09 10:26:43 -0500

    chore: remove empty test

--- packages/beacon-push-prize-distribution/__tests__/beacon-push-prize-distribution.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const beaconPushPrizeDistribution = require('..');
-const assert = require('assert').strict;
-
-assert.strictEqual(beaconPushPrizeDistribution(), 'Hello from beaconPushPrizeDistribution');
-console.info('beaconPushPrizeDistribution tests passed');
